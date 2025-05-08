ec3ea4ce33db5feb2a0a34e602baead75aa03d08
WoodProgrammer <asddeneme3@gmail.com>
2025-05-08 15:50:33 +0300

    master: policy test

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@ variable "region" {
 variable "bucket_name" {
   type        = string
   description = "The name of the S3 bucket"
-  default = "emr-woodprogrammer-managed-bucket"
+  default = "uber-hede.com"
 }
 
 variable "environment" {
